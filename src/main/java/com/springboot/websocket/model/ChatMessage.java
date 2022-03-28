4013ba8eb51751d21584561b99a7b3a787b009ba
ellyjohnmwangi <johnmwangielly@gmail.com>
2022-03-28 13:39:25 +0300

    Message model added

--- src/main/java/com/springboot/websocket/model/ChatMessage.java
+++ src/main/java/com/springboot/websocket/model/ChatMessage.java
@@ -1,4 +1,31 @@
 package com.springboot.websocket.model;
 
 public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT, LEAVE, JOIN
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
 }
