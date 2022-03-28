54e61eace0223602056f2e93ef03f4048bb0ab25
ellyjohnmwangi <johnmwangielly@gmail.com>
2022-03-28 13:39:01 +0300

    Message mappings added

--- src/main/java/com/springboot/websocket/controller/ChatController.java
+++ src/main/java/com/springboot/websocket/controller/ChatController.java
@@ -1,4 +1,22 @@
 package com.springboot.websocket.controller;
 
+import com.springboot.websocket.model.ChatMessage;
+import org.apache.logging.log4j.message.SimpleMessage;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+
 public class ChatController {
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpleMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
 }
